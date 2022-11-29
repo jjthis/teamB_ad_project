7f39227bb6a9f4a8142ff8b976cee6f82fc90051
jjthis <dfd2726@gmail.com>
2022-11-29 21:09:13 +0900

    'test'

--- selectRoom.py
+++ selectRoom.py
@@ -4,12 +4,15 @@ from PyQt5.QtWidgets import *
 class RoomSelect(QDialog, QWidget):
 
     def click(self):
+        bt = QToolButton()
+        bt.setText('asd')
+        self.mainLayout.addWidget(bt)
         pass
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        mainLayout = QVBoxLayout()
+        self.mainLayout = QVBoxLayout()
         # QVBoxLayout
         loginLabel = QLabel()
         loginLabel.setText("Mod Option Select")
@@ -19,9 +22,9 @@ class RoomSelect(QDialog, QWidget):
         button2 = QToolButton()
         button2.setText("offline")
         button2.clicked.connect(self.click)
-        mainLayout.addWidget(loginLabel)
-        mainLayout.addWidget(button)
-        mainLayout.addWidget(button2)
+        self.mainLayout.addWidget(loginLabel)
+        self.mainLayout.addWidget(button)
+        self.mainLayout.addWidget(button2)
 
-        self.setLayout(mainLayout)
+        self.setLayout(self.mainLayout)
         self.setWindowTitle('???')
